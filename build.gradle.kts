f304f6f5db0649c09c8e0363364cecc25b9bb518
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2025-04-04 19:47:15 -0700

    Initial Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+    `java-library`
+}
+
+group = "trade.invision"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Jetbrains Annotations
+    implementation("org.jetbrains", "annotations", "24.1.0")
+
+    // Google Guava
+    implementation("com.google.guava", "guava", "33.2.1-jre")
+
+    // big-math
+    implementation("ch.obermuhlner", "big-math", "2.3.2")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
