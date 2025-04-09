7762511db2a7c8dd863bf5ec8c769946a73f894f
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2025-04-08 23:53:32 -0700

    Add JReleaser Maven Central publishing via GitHub Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    `maven-publish`
 }
 
 group = "trade.invision"
@@ -13,9 +14,6 @@ dependencies {
     // Jetbrains Annotations
     implementation("org.jetbrains", "annotations", "24.1.0")
 
-    // Google Guava
-    implementation("com.google.guava", "guava", "33.2.1-jre")
-
     // big-math
     implementation("ch.obermuhlner", "big-math", "2.3.2")
 }
@@ -23,4 +21,52 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.javadoc.configure {
+    options {
+        (this as CoreJavadocOptions).addBooleanOption("Xdoclint:none", true)
+        addStringOption("link", "https://docs.oracle.com/en/java/javase/21/docs/api/")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "num"
+            from(components["java"])
+            pom {
+                name = "num"
+                description = "A Java library that abstracts the mathematical operations on real decimal numbers " +
+                        "represented in computer memory as floating-point binary numbers or " +
+                        "arbitrary-precision decimal numbers."
+                url = "https://github.com/invision-trading/num"
+                inceptionYear = "2025"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "Petersoj"
+                        name = "Jacob Peterson"
+                    }
+                }
+                scm {
+                    connection = "scm:git:https://github.com/invision-trading/num.git"
+                    developerConnection = "scm:git:ssh://github.com/invision-trading/num.git"
+                    url = "https://github.com/invision-trading/num"
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri(layout.buildDirectory.dir("staging-deploy"))
+        }
+    }
 }
